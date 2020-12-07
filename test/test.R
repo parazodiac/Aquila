a70e75087dccc1d1894378e8b31630d647108115
Avi Srivastava <asrivastava@cs.stonybrook.edu>
2020-12-06 19:29:55 -0500

    adding test

---
+++ test/test.R
@@ -0,0 +1,12 @@
+library(Matrix)
+
+obj <- readRDS("/home/haoy/ASAP/moran.i.test.rds")
+adt.set <-c("CD4-1","CD14", "CD34", "Cadherin")
+adt.value <- obj[["ADT"]]@data[adt.set, ]
+snn.graph <- obj[["wsnn"]]
+
+snn.graph <- Matrix(snn.graph, sparse = TRUE)
+adt.value <- Matrix(adt.value, sparse = TRUE)
+
+df <- MoransI(adt.value, snn.graph, "/home/srivastavaa/test_data/")
+df
